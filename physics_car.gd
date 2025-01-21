340c384c126946e30d4bdc38d93874b8ec6054f5
JVerva <joaoguedessoaresvera@gmail.com>
2025-01-21 13:03:14 +0000

    cleanup

--- physics_car.gd
+++ physics_car.gd
@@ -5,12 +5,7 @@ class_name PhysicsCar extends RigidBody2D
 @export var weapon: Weapon
 
 func _ready():
-	weapon.fired.connect(func(impulse:Vector2):
-		var perpendicular_component := get_forward_direction().rotated(-PI/2).dot(impulse)
-		var parallel_component := get_forward_direction().dot(impulse)
-		apply_central_impulse(parallel_component * get_forward_direction()/80)
-		apply_torque_impulse((perpendicular_component)*1.5)
-	)
+	weapon.fired.connect(apply_knockback)
 
 func get_forward_direction() -> Vector2:
 	return (to_global(Vector2.UP) - to_global(Vector2.ZERO))
@@ -22,3 +17,9 @@ func _physics_process(_delta):
 func apply_drift_friction():
 	var perpendicular_component := get_forward_direction().rotated(-PI/2).dot(linear_velocity)
 	apply_central_force(-get_forward_direction().rotated(-PI/2) * perpendicular_component * drift_friction_strength)
+
+func apply_knockback(impulse: Vector2) -> void:
+	var perpendicular_component := get_forward_direction().rotated(-PI/2).dot(impulse)
+	var parallel_component := get_forward_direction().dot(impulse)
+	apply_central_impulse(parallel_component * get_forward_direction()/80)
+	apply_torque_impulse((perpendicular_component)*1.5)
