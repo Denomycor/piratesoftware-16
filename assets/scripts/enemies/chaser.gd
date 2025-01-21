3b08273f05a2ea81df192d0f12e264fcc8adbd33
Vicente Sousa <vicentecsousa@gmail.com>
2025-01-21 22:23:16 +0000

    fixed hitboxes and added sample texture

--- assets/scripts/enemies/chaser.gd
+++ assets/scripts/enemies/chaser.gd
@@ -1,29 +1,23 @@
-extends Enemy
+class_name Chaser extends Enemy
 
-@export var health: int = 100
+@export var health: int = 10
 
 func _ready() -> void:
-	connect("has_taken_damage", _take_dmg)
+	hurt_box.has_taken_damage.connect(_take_dmg)
 
 
 func attack():
 	print("I am the pacifist")
 
 func update_movement():
-	print("global position: ", global_position)
-	print("target global position: ", target.global_position)
-	print("hand direction normalized position: ", (target.global_position - global_position).normalized())
-	print("target global position normalized: ", target.global_position.normalized())
-
 	var direction = global_position.direction_to(target.global_position)
-	print("Direction to target: ", direction)
 	velocity = direction * speed
-	print("Velocity set to: ", velocity)
-
 
 func die():
-	print("I am gonna die")
-	
+	queue_free()
+
 # Signal
 func _take_dmg(amount: int):
 	health -= amount
+	if health <= 0:
+		die()
--- assets/scripts/enemies/manager/enemies-manager.gd
+++ assets/scripts/enemies/manager/enemies-manager.gd
@@ -8,23 +8,16 @@ extends Node
 
 var chaser_scene = load("res://assets/scenes/enemies/chaser.tscn")
 
-var enemies: Array = []
 var cur_group = 0
 
 func _ready() -> void:
 	spawn_timer.start()
 	spawn_timer.timeout.connect(_spawn_enemy)
-
-func remove_enemy(enemy: Node) -> void:
-	if enemy in enemies:
-		enemies.erase(enemy)
-		enemy.queue_free()
-
 	
 func update_enemies():
 	cur_group = cur_group % num_groups
 
-	print("updating group: ", cur_group)
+	var enemies = get_tree().get_nodes_in_group("enemies")
 
 	for i in range(len(enemies)):
 		if i % num_groups == cur_group:
@@ -38,13 +31,8 @@ func _process(_delta: float) -> void:
 
 
 func _spawn_enemy() -> void:
-	if target == null:
-		print("Target is null")
-		return
-
 	var pos = Vector2.ZERO
 	var enemy_instance = chaser_scene.instantiate()
 	enemy_instance.target = target
 	enemy_instance.global_position = pos
 	add_child(enemy_instance)
-	enemies.append(enemy_instance)
