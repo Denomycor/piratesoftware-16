5a208f242624d5a145f69a711641983315fd7008
JVerva <joaoguedessoaresvera@gmail.com>
2025-01-21 12:55:10 +0000

    physics car good result

--- car.gd
+++ car.gd
@@ -1,12 +1,17 @@
 class_name Car extends CharacterBody2D
 
 @export var speed: float = 20
+@export var weapon: Weapon
 var target_velocity: Vector2
 var angular_speed: float
 
-func _physics_process(_delta):
+func _ready():
+	weapon.fired.connect(apply_impulse)
+
+func _physics_process(delta: float):
 	target_velocity = speed * get_forward_direction()
 	move_and_slide()
+	apply_rotation(delta)
 
 func get_forward_direction() -> Vector2:
 	return (to_global(Vector2.UP) - to_global(Vector2.ZERO))
@@ -17,3 +22,6 @@ func apply_impulse(impulse: Vector2) -> void:
 
 	velocity += parallel_component * get_forward_direction()
 	angular_speed -= perpendicular_component
+
+func apply_rotation(delta: float):
+	rotate(angular_speed * delta)
---
+++ physics_car.gd
@@ -0,0 +1,24 @@
+class_name PhysicsCar extends RigidBody2D
+
+@export var motor_strength: float
+@export var drift_friction_strength: float
+@export var weapon: Weapon
+
+func _ready():
+	weapon.fired.connect(func(impulse:Vector2):
+		var perpendicular_component := get_forward_direction().rotated(-PI/2).dot(impulse)
+		var parallel_component := get_forward_direction().dot(impulse)
+		apply_central_impulse(parallel_component * get_forward_direction()/80)
+		apply_torque_impulse((perpendicular_component)*1.5)
+	)
+
+func get_forward_direction() -> Vector2:
+	return (to_global(Vector2.UP) - to_global(Vector2.ZERO))
+
+func _physics_process(_delta):
+	apply_central_force(get_forward_direction()*motor_strength)
+	apply_drift_friction()
+
+func apply_drift_friction():
+	var perpendicular_component := get_forward_direction().rotated(-PI/2).dot(linear_velocity)
+	apply_central_force(-get_forward_direction().rotated(-PI/2) * perpendicular_component * drift_friction_strength)
