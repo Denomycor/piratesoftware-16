11379f414cfa1de6bc37cb2b9d480b2d5d278b99
JVerva <joaoguedessoaresvera@gmail.com>
2025-01-21 19:19:04 +0000

    clean up

--- car.gd
+++ car.gd
@@ -1,27 +1,32 @@
-class_name Car extends CharacterBody2D
+class_name Car extends RigidBody2D
 
-@export var speed: float = 20
+@export var motor_strength: float
+@export var drift_friction_strength: float
+@export var torque_multiplier: float
+@export var perpendicular_multiplier: float
+@export var parallel_multiplier: float
 @export var weapon: Weapon
-var target_velocity: Vector2
-var angular_speed: float
 
 func _ready():
-	weapon.fired.connect(apply_impulse)
-
-func _physics_process(delta: float):
-	target_velocity = speed * get_forward_direction()
-	move_and_slide()
-	apply_rotation(delta)
+	weapon.fired.connect(apply_knockback)
 
 func get_forward_direction() -> Vector2:
 	return (to_global(Vector2.UP) - to_global(Vector2.ZERO))
 
-func apply_impulse(impulse: Vector2) -> void:
+func get_perpendicular_direction() -> Vector2:
+	return (to_global(Vector2.UP) - to_global(Vector2.ZERO)).rotated(-PI/2)
+
+func _physics_process(_delta):
+	apply_central_force(get_forward_direction()*motor_strength)
+	apply_drift_friction()
+
+func apply_drift_friction():
+	var perpendicular_component := get_perpendicular_direction().dot(linear_velocity)
+	apply_central_force(-get_perpendicular_direction() * perpendicular_component * drift_friction_strength)
+
+func apply_knockback(impulse: Vector2) -> void:
+	var perpendicular_component := get_perpendicular_direction().dot(impulse)
 	var parallel_component := get_forward_direction().dot(impulse)
-	var perpendicular_component := get_forward_direction().rotated(PI/2).dot(impulse)
-
-	velocity += parallel_component * get_forward_direction()
-	angular_speed -= perpendicular_component
-
-func apply_rotation(delta: float):
-	rotate(angular_speed * delta)
+	apply_central_impulse(parallel_component * get_forward_direction() * parallel_multiplier)
+	apply_central_impulse(perpendicular_component * get_perpendicular_direction() * perpendicular_multiplier)
+	apply_torque_impulse(perpendicular_component * torque_multiplier)
