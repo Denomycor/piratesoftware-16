beb6adf77bd90e20ca224989edc54369cda27626
Vicente Sousa <vicentecsousa@gmail.com>
2025-01-23 12:57:07 +0000

    added types to arena.gd

--- assets/scripts/arena/arena.gd
+++ assets/scripts/arena/arena.gd
@@ -1,10 +1,9 @@
 class_name ArenaWalls extends Node2D
 
-
 @export var walls_per_side: int = 10
 
-var wall = preload("res://assets/scenes/levels/walls/Wall.tscn")
-var corner = preload("res://assets/scenes/levels/walls/Corner.tscn")
+var wall: PackedScene = preload("res://assets/scenes/levels/walls/Wall.tscn")
+var corner: PackedScene = preload("res://assets/scenes/levels/walls/Corner.tscn")
 
 
 const NUMBER_OF_SIDES: int = 8
@@ -12,27 +11,27 @@ const NUMBER_OF_SIDES: int = 8
 const WALL_LENGTH: int = 1035
 const CORNER_LENGTH: int = 1025
 
-var angle_increment = 2 * PI / NUMBER_OF_SIDES
+var angle_increment: float = 2 * PI / NUMBER_OF_SIDES
 
 func _ready() -> void:
 	build_arena()
 
-func build_arena():
+func build_arena() -> void:
 
-	var radius = walls_per_side * WALL_LENGTH + CORNER_LENGTH * (NUMBER_OF_SIDES - 2)
+	var radius: float = walls_per_side * WALL_LENGTH + CORNER_LENGTH * (NUMBER_OF_SIDES - 2)
 
-	var center = position
+	var center: Vector2 = position
 
-	var last_position = to_global(center) + Vector2(radius, 0)
+	var last_position: Vector2 = to_global(center) + Vector2(radius, 0)
 
-	var direction = Vector2(cos(angle_increment), sin(angle_increment)).rotated(PI / 2).normalized()
+	var direction: Vector2 = Vector2(cos(angle_increment), sin(angle_increment)).rotated(PI / 2).normalized()
 
 	for i in range(NUMBER_OF_SIDES):
 	
 		last_position = build_wall(last_position, direction)
 
 		
-		var corner_instance = corner.instantiate()
+		var corner_instance: Node2D = corner.instantiate()
 		corner_instance.position = last_position
 		corner_instance.rotation = direction.angle()
 		add_child(corner_instance)
@@ -43,11 +42,11 @@ func build_arena():
 
 		last_position += direction * CORNER_LENGTH
 
-func build_wall(start_position: Vector2, direction: Vector2):
-	var facing_angle = direction.rotated(PI / 2).angle()
+func build_wall(start_position: Vector2, direction: Vector2) -> Vector2:
+	var facing_angle: float = direction.rotated(PI / 2).angle()
 
 	for i in range(walls_per_side):
-		var wall_instance = wall.instantiate()
+		var wall_instance: Node2D = wall.instantiate()
 		wall_instance.position = start_position + direction * i * WALL_LENGTH
 		wall_instance.rotation = facing_angle
 		add_child(wall_instance)
