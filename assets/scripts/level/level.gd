8e5cf2fe70f012aa07e9499d3a5892fdc94225ca
JVerva <joaoguedessoaresvera@gmail.com>
2025-01-23 17:47:03 +0000

    made vars for menus in levels

--- assets/scripts/level/level.gd
+++ assets/scripts/level/level.gd
@@ -4,11 +4,14 @@ class_name Level extends Node
 @export var stats: Stats
 @export var car: Car
 
+@onready var pause_menu: PauseMenu = $PauseMenu
+@onready var game_over_menu: GameOverMenu = $GameOverMenu
+
 signal level_exited(level: Node)
 
 func _ready():
 	LevelContext.level = self
-	$PauseMenu.quit_level.connect(quit_level)
+	pause_menu.quit_level.connect(quit_level)
 
 func quit_level() -> void:
 	level_exited.emit(self)
@@ -16,6 +19,6 @@ func quit_level() -> void:
 func set_game_over() -> void:
 	get_tree().paused = true
 	stats.is_game_over = true
-	$PauseMenu.queue_free()
-	$GameOverMenu.set_stats(stats.points, stats.time_survived, stats.kills, stats.max_speed, stats.max_drift_duration)
-	$GameOverMenu.show_game_over_menu()
+	pause_menu.queue_free()
+	game_over_menu.set_stats(int(stats.points), stats.time_survived, stats.kills, stats.max_speed, stats.max_drift_duration)
+	game_over_menu.show_game_over_menu()
