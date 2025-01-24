49ea0871c1bcaf5980f2295d92d9921640fccdfd
VSousa <vicentecsousa@gmail.com>
2025-01-24 22:32:37 +0000

    Proposal for death animation (#19)
    * death animation
    
    * finished animation example
    
    * added enemies again
    
    * fixed timer time

--- assets/scripts/enemies/crawler.gd
+++ assets/scripts/enemies/crawler.gd
@@ -2,10 +2,16 @@ class_name Crawler extends Enemy
 
 @export var health: int = 10
 
+@onready var gpuParticles: GPUParticles2D = $GPUParticles2D
+@onready var animations: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision: CollisionShape2D = $CollisionShape2D
+@onready var hit_box: HitBoxComponent = $HitBoxComponent
+
 func _ready() -> void:
 	hurt_box.has_taken_damage.connect(_take_dmg)
-
-
+	animations.frame = randi() % animations.sprite_frames.get_frame_count(animations.animation)
+	animations.speed_scale = randf_range(0.8, 1.2)
+	
 func attack():
 	print("I am the pacifist")
 
@@ -16,7 +22,14 @@ func update_movement():
 
 func die():
 	died.emit()
-	queue_free()
+	velocity = Vector2.ZERO
+	hurt_box.has_taken_damage.disconnect(_take_dmg)
+	hurt_box.queue_free()
+	collision.queue_free()
+	hit_box.queue_free()
+	animations.visible = false
+	gpuParticles.emitting = true
+	gpuParticles.finished.connect(queue_free)
 
 # Signal
 func _take_dmg(amount: int):
