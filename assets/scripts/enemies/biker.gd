d82c626ba745fbf3d72ae5cb6d415d598e4261dc
VSousa <vicentecsousa@gmail.com>
2025-01-24 18:59:34 +0000

    Biker enemy (#13)
    * initial biker commit
    
    * biker almost done
    
    * temporary change
    
    * fixed name
    
    * better ai for biker
    
    * improved ai

---
+++ assets/scripts/enemies/biker.gd
@@ -0,0 +1,70 @@
+class_name Biker extends Enemy
+
+@export var health: int = 20
+@export_range(500, 1500) var follow_range: int
+@export var prediction_time: float = 0.3
+@export var max_angle_diff: float = 1.0
+
+@onready var line_of_sight: RayCast2D = $LineOfSight
+
+
+func _ready() -> void:
+	hurt_box.has_taken_damage.connect(_take_dmg)
+	if follow_range == 0:
+		follow_range = int(randf_range(500, 1500))
+
+func attack():
+	if line_of_sight.is_colliding():
+		print("I am the biker")
+
+func update_movement():
+	pass
+	
+	
+func _physics_process(delta: float) -> void:
+
+	var target_dist = target.global_position.distance_to(global_position)
+	
+	var time_to_collision = target.linear_velocity.distance_to(global_position) / target.linear_velocity.length()
+	var predicted_target_position = target.global_position + (target.linear_velocity * time_to_collision * delta)
+	
+	if velocity == Vector2.ZERO:
+		velocity = global_position.direction_to(predicted_target_position) * speed * delta
+	
+	var desired_velocity: Vector2
+	var desired_speed: float
+	
+	if target_dist > follow_range:
+		desired_velocity = global_position.direction_to(predicted_target_position)
+		desired_speed = speed
+	else:
+		desired_velocity = target.linear_velocity.normalized()
+		desired_speed = minf(speed, target.linear_velocity.length())
+	
+	var current_angle = velocity.angle()
+	var target_angle = desired_velocity.angle()
+	var angle_diff = target_angle - current_angle
+	angle_diff = clampf(angle_diff, -max_angle_diff * delta, max_angle_diff * delta)
+
+	print("max angle diff: ", max_angle_diff * delta)
+	print("current angle: ", current_angle)
+	print("target angle: ", target_angle)
+	print("angle diff: ", angle_diff)
+	print("desired velocity: ", desired_velocity)
+	print("current velocity: ", velocity)
+
+
+	velocity = velocity.rotated(angle_diff).normalized() * desired_speed
+	look_at(global_position + velocity)
+	
+	move_and_slide()
+
+
+func die():
+	queue_free()
+
+# Signal
+func _take_dmg(amount: int):
+	health -= amount
+	if health <= 0:
+		die()
--- assets/scripts/enemies/manager/enemies-manager.gd
+++ assets/scripts/enemies/manager/enemies-manager.gd
@@ -3,10 +3,16 @@ extends Node
 @export var target: RigidBody2D
 @export var num_groups: int = 10
 
+@export var max_distance: float = 20000
+@export var teleport_distance: float = 20000
+
+@export var max_enemies: int = 100
+
 @onready var spawn_timer: Timer = $SpawnTimer
 
 
 var crawler_scene = load("res://assets/scenes/enemies/crawler.tscn")
+var biker_scene = load("res://assets/scenes/enemies/biker.tscn")
 
 var cur_group = 0
 
@@ -14,34 +20,45 @@ func _ready() -> void:
 	spawn_timer.start()
 	spawn_timer.timeout.connect(_spawn_enemy)
 	
-func update_enemies():
+func _update_enemies():
 	cur_group = cur_group % num_groups
 
-	var enemies = get_tree().get_nodes_in_group("enemies")
+	var enemies = _get_enemies()
 
-	for i in range(len(enemies)):
+	for i in range(enemies.size()):
 		if i % num_groups == cur_group:
-			if enemies[i].position.distance_to(target.position) > 10000:
-				var target_dir = target.linear_velocity.normalized()
-				var teleport_dir = Vector2(randf() - 0.5, randf() - 0.5).normalized()
-
-				if target_dir.dot(teleport_dir) < 0:
-					teleport_dir = -teleport_dir
-
-				enemies[i].position = target.position + teleport_dir * 10000
+			if enemies[i].position.distance_to(target.position) > max_distance:
+				_teleport_enemy(enemies[i])
 
 			enemies[i].update_movement()
 
 	cur_group += 1
 
 
+func _teleport_enemy(enemy: Enemy) -> void:
+	var target_dir = target.linear_velocity.normalized()
+	var teleport_dir = Vector2(randf() - 0.5, randf() - 0.5).normalized()
+
+	if target_dir.dot(teleport_dir) < 0:
+		teleport_dir = -teleport_dir
+
+	enemy.position = target.position + teleport_dir * teleport_distance
+
+
 func _process(_delta: float) -> void:
-	update_enemies()
+	_update_enemies()
 
 
 func _spawn_enemy() -> void:
+	if _get_enemies().size() >= max_enemies:
+		return
+	
 	var pos = Vector2.ZERO
-	var enemy_instance = crawler_scene.instantiate()
+	var enemy_instance = biker_scene.instantiate()
 	enemy_instance.target = target
 	enemy_instance.global_position = pos
 	add_child(enemy_instance)
+
+
+func _get_enemies() -> Array:
+	return get_tree().get_nodes_in_group("enemies")
