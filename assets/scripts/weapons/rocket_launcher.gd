fdd8557342b76fd849f90ee2f7ae4702957de657
janeka <joaoaganjos@gmail.com>
2025-01-24 23:11:19 +0100

    Small correction to rocket launcher script to be consistent with new behaviour on weapon classes

--- assets/scripts/weapons/rocket_launcher.gd
+++ assets/scripts/weapons/rocket_launcher.gd
@@ -5,11 +5,18 @@ const PROJECTILE_SCENE: PackedScene = preload("res://assets/scenes/projectiles/r
 @onready var turn_component: TurnComponent = $TurnComponent
 @onready var projectile_spawner_component: ProjectileSpawnerComponent = $ProjectileSpawnerComponent
 
+@export var strength: float = 200
+
 func _ready() -> void:
 	projectile_spawner_component.shoot_projectile.connect(func(from: Vector2, rot: float, _data):
 		var projectile: LinearProjectile = PROJECTILE_SCENE.instantiate()
 		projectile.set_properties(from, rot)
-		get_node("../..").add_child(projectile)
+		
+		LevelContext.level.get_node("World").add_child(projectile)
+
+		var dir: Vector2 = get_global_mouse_position().direction_to(global_position)
+		
+		fired.emit(dir * strength)
 	)
 
 #temporary func to test shooting
