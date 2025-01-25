bda9d96d1d85daac1f6e07a7b219be9f06cb6dc4
VSousa <vicentecsousa@gmail.com>
2025-01-25 00:40:49 +0000

    add health to overlay (#22)

--- assets/scripts/level/stats.gd
+++ assets/scripts/level/stats.gd
@@ -3,7 +3,7 @@ var is_game_over: bool = false
 
 var points: float
 var time_survived: float
-var kills: int
+var kills: int = 0
 var max_speed: float
 var max_drift_duration: float
 
@@ -11,19 +11,21 @@ var max_drift_duration: float
 @export var overlay: Overlay
 
 func add_points(amount: float) -> void:
-    points += amount
-    overlay.set_points(int(points))
+	points += amount
+	overlay.set_points(int(points))
 
 func increment_kills() -> void:
-    kills += 1
+	kills += 1
+	overlay.set_kills(kills)
 
 func check_max_speed() -> void:
-    var speed = LevelContext.level.car.get_speed()
-    if  speed > max_speed:
-        max_speed = speed
+	var speed = LevelContext.level.car.get_speed()
+	if speed > max_speed:
+		max_speed = speed
 
 func _physics_process(delta: float):
-    check_max_speed()
-    if not is_game_over:
-        add_points(points_per_second * delta)
-        time_survived += delta
+	check_max_speed()
+	if not is_game_over:
+		add_points(points_per_second * delta)
+		time_survived += delta
+		overlay.set_hp(LevelContext.level.car.health)
