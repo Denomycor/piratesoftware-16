30e9a97201ae21777b76462541e0568faab78ed2
VSousa <vicentecsousa@gmail.com>
2025-01-25 23:04:41 +0000

    barrels now have visual explosion (#26)

--- assets/scripts/props/barrel.gd
+++ assets/scripts/props/barrel.gd
@@ -2,16 +2,19 @@ class_name Barrel extends Prop
 
 @onready var hit_box := $HitBoxComponent
 @onready var hurt_box := $HurtBoxComponent
+@onready var explosion_particles: GPUParticles2D = $GPUParticles2D
 
 @export var health := 40
 
 func _ready():
     hurt_box.has_taken_damage.connect(_on_take_damage)
     hit_box.monitoring = false
+    
 
 func _on_take_damage(amount: int):
     health -= amount
     if health <= 0:
         hit_box.monitoring = true
+        explosion_particles.emitting = true
         var tween := get_tree().create_tween()
-        tween.tween_callback(queue_free).set_delay(0.1)
+        tween.tween_callback(queue_free).set_delay(explosion_particles.lifetime)
