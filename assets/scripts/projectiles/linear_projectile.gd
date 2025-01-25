552a5623dd8c5a3dbdb7b82e20081b9ce863d234
Denomycor <afonsoresteves@hotmail.com>
2025-01-25 11:46:11 +0000

    Added inherited_velocity parameter to projectiles

--- assets/scripts/projectiles/linear_projectile.gd
+++ assets/scripts/projectiles/linear_projectile.gd
@@ -5,6 +5,8 @@ class_name LinearProjectile extends CharacterBody2D
 @export var lifetime: float
 
 var timer: Tween
+var travel_direction: Vector2
+var inherited_velocity := Vector2.ZERO
 
 
 func _ready() -> void:
@@ -15,12 +17,14 @@ func _ready() -> void:
 func set_properties(pos: Vector2, rot: float) -> void:
 	global_position = pos
 	rotation = rot
+	travel_direction = Vector2.from_angle(rotation)
 
 
 func _physics_process(_delta) -> void:
-	var motion := Vector2.from_angle(rotation) * speed
+	var motion := (travel_direction * speed) + inherited_velocity
 	move_and_collide(motion)
 
 
 func destroy() -> void:
 	queue_free()
+
