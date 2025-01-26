80dd0c3d571fa43d5e96a6f164a650e70f5c1154
Denomycor <afonsoresteves@hotmail.com>
2025-01-26 15:51:20 +0000

    Improved flamethrower

--- assets/scripts/projectiles/flamethrower_projectile.gd
+++ assets/scripts/projectiles/flamethrower_projectile.gd
@@ -10,11 +10,14 @@ var timer: Tween
 var travel_direction: Vector2
 var inherited_velocity := Vector2.ZERO
 
+var dying := false
+
 
 func _ready() -> void:
 	timer = create_tween()
 	timer.tween_callback(destroy).set_delay(lifetime)
 	$GPUParticles2D.emitting = true
+	$GPUParticles2D.finished.connect(queue_free)
 
 
 func set_properties(pos: Vector2, rot: float) -> void:
@@ -23,11 +26,13 @@ func set_properties(pos: Vector2, rot: float) -> void:
 
 
 func _physics_process(delta) -> void:
-	collision_shape.scale += Vector2.ONE * 0.1 
+	if(!dying):
+		collision_shape.scale += Vector2.ONE * 0.1 
 	var motion := (travel_direction * speed) + inherited_velocity
 	global_position += motion * delta
 
 
 func destroy() -> void:
-	queue_free()
+	collision_shape.set_deferred("disbled", true)
+	dying = true
 
