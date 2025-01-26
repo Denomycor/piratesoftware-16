a26da63c9056e729cb8f0f6be088db01dfca5203
janeka <joaoaganjos@gmail.com>
2025-01-27 00:09:16 +0100

    Biker shooting final implementation

--- assets/scripts/weapons/biker_gun.gd
+++ assets/scripts/weapons/biker_gun.gd
@@ -3,6 +3,7 @@ class_name BikerGun extends EnemyWeapon
 #using minigun projectile as placeholder until debate
 const PROJECTILE_SCENE: PackedScene = preload("res://assets/scenes/projectiles/minigun_projectile.tscn")
 
+@export var max_rotation: float = 90
 @onready var turn_component: EnemyTurnComponent = $TurnComponent
 @onready var projectile_spawner_component: ProjectileSpawnerComponent = $ProjectileSpawnerComponent
 
@@ -12,13 +13,25 @@ func _ready() -> void:
 		var projectile: LinearProjectile = PROJECTILE_SCENE.instantiate()
 		projectile.set_properties(from, rot)
 
+		projectile.inherited_velocity = parent.velocity
 		LevelContext.level.get_node("World").add_child(projectile)
 	)
 
 
 func _process(_delta: float) -> void:
-	if global_position.distance_to(LevelContext.level.car.global_position) < activation_range:
+	var car_position = LevelContext.level.car.global_position
+	if global_position.distance_to(car_position) < activation_range:
 		turn_component.activate()
-		projectile_spawner_component.shoot(LevelContext.level.car.global_position)
 	elif turn_component.active:
 		turn_component.deactivate()
+	
+	if turn_component.active:
+		var angle = parent.velocity.angle_to(global_position.direction_to(car_position))
+		if abs(angle) > deg_to_rad(max_rotation):
+			turn_component.lock_turn(max_rotation*sign(angle))
+			projectile_spawner_component.enabled = false
+		else:
+			projectile_spawner_component.enabled = true
+		projectile_spawner_component.shoot(car_position)
+
+	
--- assets/scripts/weapons/components/enemy_turn_component.gd
+++ assets/scripts/weapons/components/enemy_turn_component.gd
@@ -3,10 +3,12 @@ class_name EnemyTurnComponent extends Node2D
 @onready var parent: Node2D = get_parent()
 
 var active:= false
+var locked:= false
 
 func _process(_delta: float) -> void:
-	if active:
+	if active && !locked:
 		parent.look_at(LevelContext.level.car.global_position)
+	locked = false
 
 func activate() -> void:
 	active = true
@@ -15,3 +17,7 @@ func deactivate() -> void:
 	active = false
 	#Reset to original position
 	parent.rotation = 0
+
+func lock_turn(angle: float) -> void:
+	locked = true
+	parent.rotation = angle
