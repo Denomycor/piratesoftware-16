d70f02c251a695d725a49bebaf37e49ce75b56cf
JVerva <joaoguedessoaresvera@gmail.com>
2025-01-26 10:39:59 +0000

    fix barrel disapearing

--- assets/scripts/props/barrel.gd
+++ assets/scripts/props/barrel.gd
@@ -3,6 +3,8 @@ class_name Barrel extends Prop
 @onready var hit_box := $HitBoxComponent
 @onready var hurt_box := $HurtBoxComponent
 @onready var explosion_particles: GPUParticles2D = $GPUParticles2D
+@onready var sprite := $Sprite2D
+@onready var collision_shape := $CollisionShape2D
 
 @export var health := 40
 
@@ -15,6 +17,8 @@ func _on_take_damage(amount: int):
     health -= amount
     if health <= 0:
         hit_box.monitoring = true
+        sprite.visible=false
+        collision_shape.disabled = true
         explosion_particles.emitting = true
         var tween := get_tree().create_tween()
         tween.tween_callback(queue_free).set_delay(explosion_particles.lifetime)
