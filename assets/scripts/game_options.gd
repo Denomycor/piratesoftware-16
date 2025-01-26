6d9683bff165017a23eda85aa2b8fce1922bffd8
JVerva <joaoguedessoaresvera@gmail.com>
2025-01-26 13:33:56 +0000

    added ui bus

--- assets/scripts/game_options.gd
+++ assets/scripts/game_options.gd
@@ -1,7 +1,7 @@
 class_name gameOptions extends Node
 
-var sound_on: Array[bool] = [true, true, true]
-var sound_db: Array[float] = [40, 40, 40]
+var sound_on: Array[bool] = [true, true, true, true]
+var sound_db: Array[float] = [40, 40, 40, 40]
 var is_fullscreen: bool = false
 
 
