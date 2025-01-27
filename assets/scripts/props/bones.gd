8722c0f62cf518cb75df4795278097e574b59823
JVerva <joaoguedessoaresvera@gmail.com>
2025-01-27 02:14:51 +0000

    bone particles

--- assets/scripts/props/bones.gd
+++ assets/scripts/props/bones.gd
@@ -2,11 +2,18 @@ class_name Bones extends Prop
 
 @export var slow_ratio: float
 @export var slow_area: Area2D
+@export var sprites: Array[Sprite2D]
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var particles: GPUParticles2D = $GPUParticles2D
 
 func _ready():
-    slow_area.body_entered.connect(_on_collision)
+    slow_area.body_entered.connect(_on_collision.call_deferred)
     
 func _on_collision(node: Node) -> void:
     if node is Car:
         node.linear_velocity = node.linear_velocity*(1-slow_ratio)
-        queue_free()
+        for sprite in sprites:
+            sprite.visible = false
+        particles.emitting = true
+        collision_shape.disabled = true
+        particles.finished.connect(queue_free)
