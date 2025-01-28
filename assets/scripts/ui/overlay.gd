cf984fd6b1a0ad7b4611381e90ab7f26a6fc2b03
JVerva <joaoguedessoaresvera@gmail.com>
2025-01-28 12:55:09 +0000

    speed_pointer

--- assets/scripts/ui/overlay.gd
+++ assets/scripts/ui/overlay.gd
@@ -3,7 +3,7 @@ class_name Overlay extends CanvasLayer
 @onready var point_counter: Label = %Points
 @onready var kills_counter: Label = %Kills
 @onready var hp_counter: Label = %Hp
-@onready var speed_counter: Label = %Speed
+@export var speed_counter: SpeedPointer
 
 func set_points(points: int) -> void:
 	point_counter.text = "Points: " + str(points)
@@ -12,8 +12,8 @@ func set_kills(kills: int) -> void:
 	kills_counter.text = "Kills: " + str(kills)
 
 func set_speed(speed: float) -> void:
-	speed_counter.text = str(speed) + " km/h"
+	speed_counter.set_speed(speed)
 
 func set_hp(hp: float) -> void:
-	hp_counter.text = "HP: " + str(int(hp))
+	pass
 
