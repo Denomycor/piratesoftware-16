3a55a4aa86766f3f7af253fc7d3899efc0e532e9
JVerva <joaoguedessoaresvera@gmail.com>
2025-01-28 13:28:30 +0000

    health_bar working

--- assets/scripts/ui/overlay.gd
+++ assets/scripts/ui/overlay.gd
@@ -2,8 +2,8 @@ class_name Overlay extends CanvasLayer
 
 @onready var point_counter: Label = %Points
 @onready var kills_counter: Label = %Kills
-@onready var hp_counter: Label = %Hp
 @export var speed_counter: SpeedPointer
+@export var health_bar: HealthBar
 
 func set_points(points: int) -> void:
 	point_counter.text = "Points: " + str(points)
@@ -15,5 +15,4 @@ func set_speed(speed: float) -> void:
 	speed_counter.set_speed(speed)
 
 func set_hp(hp: float) -> void:
-	pass
-
+	health_bar.set_hp(hp)
