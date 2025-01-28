8ae0faa75f1d5539bd1c3541711de29c5c467a83
JVerva <joaoguedessoaresvera@gmail.com>
2025-01-28 18:06:33 +0000

    vignette

--- assets/scripts/ui/overlay.gd
+++ assets/scripts/ui/overlay.gd
@@ -5,6 +5,7 @@ class_name Overlay extends CanvasLayer
 @export var speed_counter: SpeedPointer
 @export var health_bar: HealthBar
 @export var weapon_bar: WeaponBar
+@export var vignette: ColorRect
 
 func set_points(points: int) -> void:
 	point_counter.text = "Points: " + str(points)
@@ -16,6 +17,7 @@ func set_speed(speed: float) -> void:
 	speed_counter.set_speed(speed)
 
 func set_hp(hp: float) -> void:
+	vignette.material.set_shader_parameter("inner_radius",lerpf(0, 1, hp/LevelContext.level.car.max_health))
 	health_bar.set_hp(hp)
 
 func switch_weapon(idx: int) -> void:
