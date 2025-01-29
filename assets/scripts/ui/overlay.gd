bbc678adda74b282e4b1cdb54ab66f01631d1a48
Vicente Sousa <vicentecsousa@gmail.com>
2025-01-29 17:33:00 +0000

    ui now supports scaling

--- assets/scripts/ui/overlay.gd
+++ assets/scripts/ui/overlay.gd
@@ -6,7 +6,18 @@ class_name Overlay extends CanvasLayer
 @export var health_bar: HealthBar
 @export var weapon_bar: WeaponBar
 @export var vignette: ColorRect
+@export var ui_scale: float = 1.0
 
+@onready var stats: VBoxContainer = $StatsContainer
+@onready var bar_container: CenterContainer = $BarContainer
+@onready var sonar_container: HBoxContainer = $SonarContainer
+
+
+func _ready() -> void:
+	stats.scale = Vector2(ui_scale, ui_scale)
+	bar_container.scale = Vector2(ui_scale, ui_scale)
+	sonar_container.scale = Vector2(ui_scale, ui_scale)
+	
 func set_points(points: int) -> void:
 	point_counter.text = "Points: " + str(points)
 
@@ -17,7 +28,7 @@ func set_speed(speed: float) -> void:
 	speed_counter.set_speed(speed)
 
 func set_hp(hp: float) -> void:
-	vignette.material.set_shader_parameter("inner_radius",lerpf(0, 1, hp/LevelContext.level.car.max_health))
+	vignette.material.set_shader_parameter("inner_radius", lerpf(0, 1, hp / LevelContext.level.car.max_health))
 	health_bar.set_hp(hp)
 
 func switch_weapon(idx: int) -> void:
