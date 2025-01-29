347c7d680e9de4e1a66a3fedf301d80fd2761c58
JVerva <joaoguedessoaresvera@gmail.com>
2025-01-29 00:43:46 +0000

    hugger is now better

--- assets/scripts/enemies/crawler.gd
+++ assets/scripts/enemies/crawler.gd
@@ -4,36 +4,34 @@ class_name Crawler extends Enemy
 @export var attack_range: float = 310
 
 @onready var gpu_particles: GPUParticles2D = $GPUParticles2D
-@onready var animations: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
 @onready var hit_box: HitBoxComponent = $HitBoxComponent
 @onready var attack_timer: Timer = $AttackCooldown
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
 
 var dead := false
-var can_attack := true
+var is_on_cooldown := false
 
 func _ready() -> void:
+	animation_player.play("stoped")
 	hit_box.monitoring = false
 	hurt_box.has_taken_damage.connect(_take_dmg)
-	animations.frame = randi() % animations.sprite_frames.get_frame_count(animations.animation)
-	animations.speed_scale = randf_range(0.8, 1.2)
-	attack_timer.timeout.connect(func(): can_attack = true)
+	attack_timer.timeout.connect(func(): is_on_cooldown = false)
 	
 func attack():
-	hit_box.monitoring = true
-	var tween := get_tree().create_tween()
-	tween.tween_callback(func(): hit_box.monitoring = false).set_delay(0.1)
-	can_attack = false
+	animation_player.play("attacking")
+	is_on_cooldown = true
 	attack_timer.start()
 
 func update_movement():
 	if dead:
 		return
-	var distance := (target.global_position - global_position).length()
-	var direction = global_position.direction_to(target.global_position)
-	look_at(target.global_position)
-	velocity = direction * speed
-	if distance < attack_range and can_attack:
+	if not is_in_range():
+		var direction = global_position.direction_to(target.global_position)
+		look_at(target.global_position)
+		velocity = direction * speed
+	if can_attack():
 		attack()
 
 func die():
@@ -45,13 +43,30 @@ func die():
 	hurt_box.queue_free()
 	collision.queue_free()
 	hit_box.queue_free()
-	animations.visible = false
+	sprite.visible = false
 	gpu_particles.emitting = true
 	gpu_particles.finished.connect(queue_free)
 	died.emit()
+
+func _process(_delta):
+	if dead:
+		return
+	if animation_player.current_animation == "attacking":
+		return
+	if velocity.length() > 20:
+		animation_player.play("crawling")
+	else:
+		animation_player.play("stoped")
 
 # Signal
 func _take_dmg(amount: float):
 	health -= amount
 	if health <= 0:
 		die()
+
+func can_attack() -> bool:
+	return is_in_range() and !is_on_cooldown
+
+func is_in_range() -> bool:
+	var distance := (target.global_position - global_position).length()
+	return distance < attack_range
