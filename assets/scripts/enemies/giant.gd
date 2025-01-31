66132f0d0fb75b5f1f606f63f0f77d530bee6710
Denomycor <afonsoresteves@hotmail.com>
2025-01-31 01:49:13 +0000

    Giant do be giant

--- assets/scripts/enemies/giant.gd
+++ assets/scripts/enemies/giant.gd
@@ -1,9 +1,10 @@
 class_name Giant extends Enemy
 
+const PROJ_SCENE := preload("res://assets/scenes/projectiles/enemy_minigun_projectile.tscn")
 
 @export var health: float = 1000
 @export var max_accelaration: float = 100000
-@export var attack_range: float = 310
+@export var melee_attack_range: float = 310
 
 @export var charge_time := 3.0
 @export var charge_speed := 1500
@@ -18,6 +19,10 @@ var charge_is_on_cooldown := true
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var attack_timer: Timer = $attack_timer
 @onready var charge_timer: Timer = $attack_timer
+@onready var projectile_spawner_component: ProjectileSpawnerComponent = $ProjectileSpawnerComponent
+
+@export var ranged_attack_range := 2000
+
 
 var dead := false
 var attack_is_on_cooldown := false
@@ -31,35 +36,47 @@ func _ready() -> void:
 	hurt_box.has_taken_damage.connect(_take_dmg)
 	hit_box.monitoring = false
 
+	projectile_spawner_component.shoot_projectile.connect(func(from: Vector2, rot: float, _data):
+		var projectile: LinearProjectile = PROJ_SCENE.instantiate()
+		projectile.set_properties(from, rot)
+		LevelContext.level.get_node("World").add_child(projectile)
+	)
+
 
 func update_movement():
 	if dead:
 		return
-	if not is_in_range(attack_range):
-		if(charge_is_on_cooldown || !is_in_range(charge_range)):
-			set_chase_acceleration()
-			look_at(global_position + velocity)
-		else:
-			start_charge()
-	else:
+	if can_attack_melee() && animation_player.current_animation != "firing":
 		look_at(target.global_position)
 		velocity = Vector2(0,0)
-	if can_attack():
-		attack()
+		attack_melee()
+	elif can_attack_ranged() && animation_player.current_animation != "attacking2" :
+		look_at(target.global_position)
+		velocity = Vector2(0,0)
+		attack_ranged()
+	elif(animation_player.current_animation != "firing" && animation_player.current_animation != "attacking2"):
+		move()
+
+
+func move() -> void:
+	if(charge_is_on_cooldown || !is_in_range(charge_range)):
+		set_chase_acceleration()
+		look_at(global_position + velocity)
+	else:
+		start_charge()
 
 
 func _physics_process(delta: float) -> void:
-	if(dead):
-		return
+	if(!dead):
+		if(!movement_locked):
+			velocity += acceleration * delta
+			move_and_slide()
 
-	if(!movement_locked):
-		velocity += acceleration * delta
-		move_and_slide()
 
 func _process(_delta: float):
 	if dead:
 		return
-	if animation_player.current_animation == "attacking2":
+	if animation_player.current_animation == "attacking2" || animation_player.current_animation == "firing":
 		return
 	if velocity.length() > 20:
 		animation_player.play("walking")
@@ -96,20 +113,33 @@ func is_in_range(frange: float) -> bool:
 	return distance < frange
 
 
-func can_attack() -> bool:
-	return is_in_range(attack_range) and !attack_is_on_cooldown
+func can_attack_melee() -> bool:
+	return is_in_range(melee_attack_range) && !attack_is_on_cooldown
 
 
-func attack() -> void:
-	print("I attacked")
+func can_attack_ranged() -> bool:
+	return is_in_range(ranged_attack_range) && projectile_spawner_component.proj_ready
+
+
+func attack_melee() -> void:
 	attack_is_on_cooldown = true
 	animation_player.play("attacking2")
 	attack_timer.start()
 
 
+func attack_ranged() -> void:
+	animation_player.play("firing")
+
+
+func _animation_synced_shot() -> void:
+	# Ensure proj shoots
+	projectile_spawner_component.proj_ready = true
+	projectile_spawner_component.shoot(target.global_position)
+
+
 func end_charge() -> void:
 	$Sprite2D.self_modulate = Color.WHITE
-	# animation_player.speed_scale = 0.6
+	animation_player.speed_scale = 0.6
 	speed = speed_backup
 	charge_timer.start()
 
@@ -118,7 +148,13 @@ func start_charge() -> void:
 	charge_is_on_cooldown = true
 	create_tween().tween_callback(end_charge).set_delay(charge_time)
 	$Sprite2D.self_modulate = charge_color
-	# animation_player.speed_scale = 1
+	animation_player.speed_scale = 1
 	speed_backup = speed
 	speed = charge_speed
+
+
+func play_cracks() -> void:
+	$Node/crack.global_position = global_position
+	$Node/AnimationPlayer.play("crack_start")
+	create_tween().tween_callback($Node/AnimationPlayer.play.bind("crack_end")).set_delay(3)
 
